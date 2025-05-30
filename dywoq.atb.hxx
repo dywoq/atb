a1198119c1d87b0abef041329e59ed9b287f7826
dywoq <aleks.koyf@gmail.com>
2025-05-30 21:16:56 +0300

    [dywoq.atb.hxx] add macros for namespaces

--- dywoq.atb.hxx
+++ dywoq.atb.hxx
@@ -2,52 +2,63 @@
 #define DYWOQATB_HXX
 
 #ifdef __dywoqatb
-  #define DYWOQATB_VERSION __dywoqatb
+#  define DYWOQATB_VERSION __dywoqatb
 #else
-  #define DYWOQATB_VERSION 202505LL
+#  define DYWOQATB_VERSION 202505LL
 #endif
 
 #ifdef __cplusplus
-  #if __cplusplus >= 202002LL
-    #define DYWOQATB_HAS_CXX20_SUPPORT 1
-  #else
-    #define DYWOQATB_HAS_CXX20_SUPPORT 0
-  #endif
+#  if __cplusplus >= 202002LL
+#    define DYWOQATB_HAS_CXX20_SUPPORT 1
+#  else
+#    define DYWOQATB_HAS_CXX20_SUPPORT 0
+#  endif
 #else
-  #error                                                                       \
+#  error                                                                       \
       "[dywoq.atb.hxx] critical error: the code must be compiled by C++ compiler"
 #endif
 
 #if DYWOQATB_HAS_CXX20_SUPPORT && DYWOQATB_VERSION >= 202505LL
-  #define DYWOQATB_HAS_BASE_SUPPORT 1
+#  define DYWOQATB_HAS_BASE_SUPPORT 1
 #else
-  #define DYWOQATB_HAS_BASE_SUPPORT 0
+#  define DYWOQATB_HAS_BASE_SUPPORT 0
 #endif
 
 #if defined(__clang__) || defined(__GNUC__)
-  #ifdef __cpp_exceptions
-    #define DYWOQATB_HAS_EXCEPTIONS 1
-  #else
-    #define DYWOQATB_HAS_EXCEPTIONS 0
-  #endif
+#  ifdef __cpp_exceptions
+#    define DYWOQATB_HAS_EXCEPTIONS 1
+#  else
+#    define DYWOQATB_HAS_EXCEPTIONS 0
+#  endif
 #elif defined(_MSC_VER)
-  #ifdef _HAS_EXCEPTIONS
-    #if _HAS_EXCEPTIONS
-      #define DYWOQATB_HAS_EXCEPTIONS 1
-    #elif
-      #define DYWOQATB_HAS_EXCEPTIONS 0
-    #endif
-  #endif
+#  ifdef _HAS_EXCEPTIONS
+#    if _HAS_EXCEPTIONS
+#      define DYWOQATB_HAS_EXCEPTIONS 1
+#    elif
+#      define DYWOQATB_HAS_EXCEPTIONS 0
+#    endif
+#  endif
 #else
-  #if defined(__clang__) || defined(__GNUC__)
-    #warning                                                                   \
+#  if defined(__clang__) || defined(__GNUC__)
+#    warning                                                                   \
         "[dywoq.atb.hxx] warning: there's no macro for tracking exceptions' status; the value of DYWOQATB_HAS_EXCEPTIONS will be always 1 "
-    #define DYWOQATB_HAS_EXCEPTIONS 1
-  #elif defined(_MSC_VER)
-    #pragma message(                                                           \
+#    define DYWOQATB_HAS_EXCEPTIONS 1
+#  elif defined(_MSC_VER)
+#    pragma message(                                                           \
         "[dywoq.atb.hxx] warning: there's no macro for tracking exceptions' status; the value of DYWOQATB_HAS_EXCEPTIONS will be always 1 ")
-    #define DYWOQATB_HAS_EXCEPTIONS 1
-  #endif
+#    define DYWOQATB_HAS_EXCEPTIONS 1
+#  endif
+#endif
+
+#define DYWOQATB_BEGIN_NAMESPACE namespace dywoq::atb {
+#define DYWOQATB_END_NAMESPACE }
+
+#if DYWOQATB_HAS_BASE_SUPPORT
+DYWOQATB_BEGIN_NAMESPACE
+
+
+
+DYWOQATB_END_NAMESPACE
 #endif
 
 #endif
